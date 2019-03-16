25184d059d8612db64b49d46ee78c9cb01da9f52
JaySon-Huang <jayson.hjs@gmail.com>
2019-03-16 16:04:15 +0800

    添加输入文件转换脚本

---
+++ conv.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+#encoding=utf-8
+
+from __future__ import print_function
+
+import os
+import sys
+
+def conv(filename, outfile):
+    with open(filename, 'r') as infile, open(outfile, 'wb') as outfile:
+        for lineno, line in enumerate(infile):
+            _, url = line.strip().split(',')  # 提取出 url
+            if lineno < 10:
+                num_repeat = (1 << (16 - lineno))
+                for _ in range(num_repeat):
+                    print(url, file=outfile)
+            else:
+                print(url, file=outfile)
+
+
+if __name__ == '__main__':
+    # parse args
+    import argparse
+    parser = argparse.ArgumentParser(description='input files convert')
+    parser.add_argument('--filename', required=True,
+                        help='url file to process')
+    parser.add_argument('--out', required=True, help='url file')
+    args = parser.parse_args()
+
+    conv(args.filename, args.out)
