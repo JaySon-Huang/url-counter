31bd1430db557a62a21a15e0648ce11f483ea194
JaySon-Huang <jayson.hjs@gmail.com>
2019-03-16 17:47:10 +0800

    数据倾斜的分析 && 解决

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # url-counter
 
 ## 问题
-100GB的url，统计 Top 100 url及其出现次数。
+100GB的url，统计 Top 100 url及其出现次数。  
 内存限制：1GB
 
 ## 解决思路
@@ -19,21 +19,36 @@ sc.readText("/path/to/input")
 
 ### 通过编程实现
 
-* 对 url hash 拆分为互不重叠的小文件
-* 各个小文件分别统计
-* 合并选出Top 1000的url
+1. 对 url hash 拆分为互不重叠的小文件
+2. 各个小文件分别统计
+3. 合并选出Top 1000的url
 
 ## 问题 && 优化
 
-* 拆分过程可能存在数据倾斜
-* 分别统计的具体方法
+### 分别统计的具体方法
   * hash-map 计数
   * trie-tree 计数
-  * 快排后，遍历计数
+  * 排序后，遍历计数
 
-## 测试数据生成
+
+### 数据倾斜 (skew)
+#### 影响分析
+数据倾斜导致 第1步 拆分得到的小文件大小不均, 从而导致第2步 统计过程, 如果利用多核并发执行, 会有少量任务所需时间比其它任务长。  
+
+* 如果第2步采用 `hash-map`/`trie-tree` 统计，数据倾斜对 **内存限制** 这一条件没有影响，因为大量key重复出现才会导致数据倾斜。而统计过程中顺序读文件，插入 hash-map/trie-tree 所需的内存不会比其它小分片大
+* 如果第2步采用排序的方式，快排就不合适，需要改为使用基于磁盘的归并排序。
+
+#### 数据倾斜的解决思路
+* 第2步为 `hash-map`/`trie-tree`
+对原数据加随机前缀后再进行一次 hash 拆分。注意拆分后合并阶段，因为同一条url可能被分到不同的文件中，需要先合并统计结果。
+* 第2步为排序
+采用基于磁盘的归并排序
+
+## 测试数据生成 && 运行
 
 ```bash
 wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 uzip top-1m.csv.zip
+python2 conv.py --filename top-1m.csv --out top-1m.url
+python2 solve.py --filename top-1m.url --split 2 --ntop 100
 ```
--- solve.py
+++ solve.py
@@ -9,6 +9,7 @@ import shutil
 import math
 import Queue
 import logging
+import random
 
 # pip install mmh3
 import mmh3
@@ -16,7 +17,8 @@ import mmh3
 import pygtrie as trie
 
 
-def part(filename, splited_mb=512):
+def part(filename, splited_mb=512, preserve_ori_file=True,
+         is_skew_add_prefix=False, skew_split_factor=10):
     # 计算拆分为多少份
     split_bytes = splited_mb * (1024 * 1024)
     file_size_to_split = os.path.getsize(filename)
@@ -26,7 +28,7 @@ def part(filename, splited_mb=512):
     ))
 
     # 生成拆分的文件夹及小文件
-    dirname = './{}-parted'.format(filename)
+    dirname = '{}-parted'.format(filename)
     if os.path.exists(dirname) and os.path.isdir(dirname):
         shutil.rmtree(dirname)
     os.mkdir(dirname)
@@ -37,47 +39,72 @@ def part(filename, splited_mb=512):
     outfiles = list(map(lambda fn: open(fn, 'wb'), outfiles_name))
     outfiles_size = [0] * num_to_split
 
-    # murmurhash3 拆分为互不重叠的小文件
+    # url 利用 murmurhash3 拆分为互不重叠的小文件
     with open(filename, 'r') as infile:
         for line in infile:
             line_len = len(line)
             line = line.strip()
-            h = mmh3.hash(line) % num_to_split
+            if is_skew_add_prefix:
+                # 处理导致数据倾斜的 skew-key, 加随机前缀 hash
+                # note, 会导致同一个 url hash 到不同文件中, 计算时需要特殊处理
+                prefix = chr(ord('a') + random.randint(0, skew_split_factor))
+                h = mmh3.hash(prefix + line) % num_to_split
+            else:
+                h = mmh3.hash(line) % num_to_split
             print(line, file=outfiles[h])
             outfiles_size[h] += line_len
     map(lambda x: x.close(), outfiles)  # 关闭文件句柄
 
     # 检查 outfiles_size 过滤出比预期大的文件 split_bytes
-    skew_files = filter(
+    skew_files_with_sz = filter(
         lambda (fn, sz): sz > split_bytes, 
         zip(outfiles_name, outfiles_size)
     )
-    if len(skew_files) > 0:
-        # TODO 对数据倾斜的部分进一步拆分
-        logging.warning('skew files: {}'.format(skew_files))
-        pass
-
+    if len(skew_files_with_sz) > 0:
+        # 对数据倾斜的部分进一步拆分。
+        logging.warning('skew files: {}'.format(skew_files_with_sz))
+        skew_files = map(lambda (fn, sz): fn, skew_files_with_sz)
+        for f in skew_files:
+            part(f, splited_mb, preserve_ori_file=False, is_skew_add_prefix=True)
+    if not preserve_ori_file:
+        logging.debug('remove origin file(skew file): {}'.format(filename))
+        os.remove(filename)
     return dirname
 
 
-def count(filename, counter_type='dict'):
+def count(fn, counter_type='dict'):
+
+    if not os.path.isdir(fn):
+        # hash 取模拆分的文件，URL 互不重叠
+        filenames = [fn]
+    else:
+        # skew key 进一步拆分的文件夹, 同一个 url 可能分布在该文件夹下的不同文件, 需要合并
+        skew_splited_files = []
+        get_leave_files(fn, skew_splited_files)
+        logging.debug('skew-splited files: {}'.format(skew_splited_files))
+        filenames = skew_splited_files
+
     if counter_type == 'dict':
         counter = {}
     elif counter_type == 'trie':
         counter = trie.CharTrie()
     else:
         raise NotImplementedError('invalid counter type: {}'.format(counter_type))
-    logging.info('counting `{}` using type: {}'.format(filename, counter_type))
-    with open(filename, 'r') as infile:
-        for lineno, line in enumerate(infile):
-            if lineno % 50000 == 0:
-                logging.debug('processing {} line'.format(lineno))
-            url = line.strip()
-            # 更新计数
-            prev_cnt = counter.get(url, 0)
-            counter[url] = prev_cnt + 1
+
+    # TODO 多线程并发
+    for filename in filenames:
+        logging.info('counting `{}` using type: {}'.format(filename, counter_type))
+        with open(filename, 'r') as infile:
+            for lineno, line in enumerate(infile):
+                if lineno % 50000 == 0:
+                    logging.debug('processing {} line'.format(lineno))
+                url = line.strip()
+                # 更新计数
+                prev_cnt = counter.get(url, 0)
+                counter[url] = prev_cnt + 1
+
     logging.info('dump counting into file...')
-    with open(filename + '_cnt', 'wb') as outfile:
+    with open(fn + '_cnt', 'wb') as outfile:
         for k in counter:
             print('{},{}'.format(k, counter[k]), file=outfile)
     logging.info('dump done')
@@ -121,6 +148,16 @@ def reduce_counters(dirname, topN=1000):
                     # 未满, 入堆
                     q.put(Item(url, cnt))
     return q
+
+
+def get_leave_files(path, file_lst):
+    cur_file_lst = os.listdir(path)
+    for fn in cur_file_lst:
+        filepath = os.path.join(path, fn)
+        if os.path.isdir(filepath):
+            get_leave_files(filepath, file_lst)
+        else:
+            file_lst.append(filepath)
 
 
 if __name__ == '__main__':
